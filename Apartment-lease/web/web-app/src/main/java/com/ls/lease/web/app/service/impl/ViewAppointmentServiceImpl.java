8bdd71225bcda1d930047acf740090426019e5bc
hnsqls <2661108038@qq.com>
2024-07-24 18:29:03 +0800

    update

--- Apartment-lease/web/web-app/src/main/java/com/ls/lease/web/app/service/impl/ViewAppointmentServiceImpl.java
+++ Apartment-lease/web/web-app/src/main/java/com/ls/lease/web/app/service/impl/ViewAppointmentServiceImpl.java
@@ -1,10 +1,20 @@
 package com.ls.lease.web.app.service.impl;
 
+import com.ls.lease.model.entity.ApartmentInfo;
 import com.ls.lease.model.entity.ViewAppointment;
+import com.ls.lease.web.app.mapper.ApartmentInfoMapper;
 import com.ls.lease.web.app.mapper.ViewAppointmentMapper;
+import com.ls.lease.web.app.service.ApartmentInfoService;
 import com.ls.lease.web.app.service.ViewAppointmentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ls.lease.web.app.vo.apartment.ApartmentItemVo;
+import com.ls.lease.web.app.vo.appointment.AppointmentDetailVo;
+import com.ls.lease.web.app.vo.appointment.AppointmentItemVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author liubo
@@ -15,6 +25,39 @@ import org.springframework.stereotype.Service;
 public class ViewAppointmentServiceImpl extends ServiceImpl<ViewAppointmentMapper, ViewAppointment>
         implements ViewAppointmentService {
 
+    @Autowired
+    private  ViewAppointmentMapper viewAppointmentMapper;
+
+    @Autowired
+    private ApartmentInfoService apartmentInfoService;
+
+    /**
+     * 根据用户id查询预约信息列表
+     * @param userId
+     * @return
+     */
+    @Override
+    public List<AppointmentItemVo> listItem(Long userId) {
+
+        return viewAppointmentMapper.listItem(userId);
+    }
+
+    /**
+     * 根据预约id查询预约详细信息
+     * @param id
+     * @return
+     */
+    @Override
+    public AppointmentDetailVo getDetailById(Long id) {
+        ViewAppointment viewAppointment = viewAppointmentMapper.selectById(id);
+        //根据公寓id查询详细公寓信息
+        ApartmentItemVo apartmentItemVo = apartmentInfoService.selectApartmentItemVoById(viewAppointment.getApartmentId());
+
+        AppointmentDetailVo appointmentDetailVo = new AppointmentDetailVo();
+        BeanUtils.copyProperties(viewAppointment,appointmentDetailVo);
+        appointmentDetailVo.setApartmentItemVo(apartmentItemVo);
+        return appointmentDetailVo;
+    }
 }
 
 
